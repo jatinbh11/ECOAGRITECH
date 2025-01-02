82986d1ff5a4b728e71a70d8939b23bd785c956e
Jatin Bhargava <127046630+jatinbh11@users.noreply.github.com>
2025-01-02 23:16:51 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -70,10 +70,9 @@ disease_model.eval()
 
 # Loading crop recommendation model
 
-crop_recommendation_model_path = 'app\models\RandomForest.pkl'
-crop_recommendation_model = pickle.load(
-    open(crop_recommendation_model_path, 'rb'))
 
+crop_recommendation_model_path = os.path.join('app', 'models', 'RandomForest.pkl')
+crop_recommendation_model = pickle.load(open(crop_recommendation_model_path, 'rb'))
 
 # =========================================================================================
 
